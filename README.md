21a9fdd3a5c12fb50483d0ddd5fd42af84fbfbf5
Rareș <raresnita@outlook.com>
2023-01-06 02:13:43 +0200

    Rewrite functions and various changes

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # JS-DateAndTime
+
 Simple webpage displaying the local time and date
 
-
 ## How does it look like?
+
 ![image](https://user-images.githubusercontent.com/85163618/189519910-f79abc8d-a3e3-4b33-89a2-8b15322b9380.png)
 For this project, I'm using a set of simple, pastel-like colours (will probably be changed in the future), and an easy-to-read font.
+At the moment, this is only available in Romanian.
 
 ## Things to do:
+
+- support for multiple languages
+- colour toggle
 - restyling dark mode button and divider line
 - add 12/24 hour switch
-- different animations (if possible)
+- animations (if possible)
--- script.js
+++ script.js
@@ -1,118 +1,51 @@
 // "use strict";
 
-document
-	.getElementById("darkModeButton")
-	.addEventListener("click", function () {
-		document.body.classList.toggle("darkMode");
+// document
+//   .getElementById("darkModeButton")
+//   .addEventListener("click", function () {
+//     document.body.classList.toggle("darkMode");
 
-		let text = document.getElementById("darkModeButton");
-		if (text.innerHTML === "clear_night") {
-			text.innerHTML = "brightness_5";
-			text.classList.toggle("lightModeButton");
-		} else {
-			text.innerHTML = "clear_night";
-		}
-	});
+//     let text = document.getElementById("darkModeButton");
+//     if (text.innerHTML === "clear_night") {
+//       text.innerHTML = "brightness_5";
+//       text.classList.toggle("lightModeButton");
+//     } else {
+//       text.innerHTML = "clear_night";
+//     }
+//   });
 
-function lightModeIcon() {
-	document.getElementById("darkModeButton").innerHTML = "clear_night";
-}
+// let lightModeIcon = function () {
+//   document.getElementById("darkModeButton").innerHTML = "clear_night";
+// };
+
+let changeColours = function () {};
 
 function currentTime() {
-	let date = new Date();
-	let hour = date.getHours();
-	let minutes = date.getMinutes();
-	let seconds = date.getSeconds();
+  let date = new Date();
+  let hour = ("0" + date.getHours()).slice(-2);
+  let minutes = ("0" + date.getMinutes()).slice(-2);
+  let seconds = ("0" + date.getSeconds()).slice(-2);
 
-	hour = hour < 10 ? "0" + hour : hour;
-	minutes = minutes < 10 ? "0" + minutes : minutes;
-	seconds = seconds < 10 ? "0" + seconds : seconds;
+  let time = hour + ":" + minutes + ":" + seconds;
 
-	let time = hour + ":" + minutes + ":" + seconds;
+  document.getElementById("timeText").innerText = time;
+  document.getElementById("timeText").textContent = time;
 
-	document.getElementById("timeText").innerText = time;
-	document.getElementById("timeText").textContent = time;
-
-	setTimeout(currentTime, 1000);
+  setTimeout(currentTime, 1000);
 }
 
 function currentDate() {
-	let date = new Date();
-	let dayOfWeek = date.getUTCDay();
-	let day = date.getUTCDate() + 1;
-	let month = date.getUTCMonth();
+  let date = new Date();
+  let day = date.getUTCDate() + 1;
+  let dayName = date.toLocaleDateString("ro-RO", { weekday: "long" });
+  let monthName = date.toLocaleString("ro-RO", { month: "long" });
 
-	let dayName, monthName;
+  let completeDate = `Astăzi este ${dayName}, ${day} ${monthName}`;
 
-	switch (dayOfWeek) {
-		case 0:
-			dayName = "luni";
-			break;
-		case 1:
-			dayName = "marți";
-			break;
-		case 2:
-			dayName = "miercuri";
-			break;
-		case 3:
-			dayName = "joi";
-			break;
-		case 4:
-			dayName = "vineri";
-			break;
-		case 5:
-			dayName = "sâmbătă";
-			break;
-		case 6:
-			dayName = "duminică";
-			break;
-	}
+  document.getElementById("dateText").innerText = completeDate;
+  document.getElementById("dateText").textContent = completeDate;
 
-	switch (month) {
-		case 0:
-			monthName = "ianuarie";
-			break;
-		case 1:
-			monthName = "februarie";
-			break;
-		case 2:
-			monthName = "martie";
-			break;
-		case 3:
-			monthName = "aprilie";
-			break;
-		case 4:
-			monthName = "mai";
-			break;
-		case 5:
-			monthName = "iunie";
-			break;
-		case 6:
-			monthName = "iulie";
-			break;
-		case 7:
-			monthName = "august";
-			break;
-		case 8:
-			monthName = "septembrie";
-			break;
-		case 9:
-			monthName = "octombrie";
-			break;
-		case 10:
-			monthName = "noiembrie";
-			break;
-		case 11:
-			monthName = "decembrie";
-			break;
-	}
-
-	let completeDate = `Astăzi este ${dayName}, ${day} ${monthName}`;
-
-	document.getElementById("dateText").innerText = completeDate;
-	document.getElementById("dateText").textContent = completeDate;
-
-	setTimeout(currentDate, 1000);
+  setTimeout(currentDate, 1000);
 }
 
 currentTime();
